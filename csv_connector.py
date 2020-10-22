b03f19d944f9e6f0156e8e01333211795d843fcd
Harald Haugerud <harald.haugerud@makingwaves.com>
2020-10-22 15:35:38 +0200

    black

--- csv_connector.py
+++ csv_connector.py
@@ -10,6 +10,7 @@ from datetime import datetime
 from elasticsearch import Elasticsearch
 from elasticsearch import helpers
 
+
 class CSVConnector:
     FILENAME = "data/alerts_table_hullkeeper.csv"
     ONLY_ONE_DATAROW = True
@@ -18,27 +19,27 @@ class CSVConnector:
         self.elasticindexer = ElasticIndexer()
         self.log = logging.getLogger("csv_connector")
 
-    def run(self): 
+    def run(self):
         self.log.info(f"Reading file: {self.FILENAME}")
         self.read_cvs_file(self.FILENAME)
-        
+
     def read_cvs_file(self, filename):
         with open(filename) as csv_file:
             reader = csv.DictReader(csv_file)
             for line in reader:
                 self.create_doc(line)
-        
+
         self.elasticindexer.index_documents()
 
     def create_doc(self, doc_row):
-        #Splits every row with data into separate documents
+        # Splits every row with data into separate documents
         es_doc = {}
 
-        #Create geofield
+        # Create geofield
         if "Latitude" in doc_row and "Longitude" in doc_row:
             es_doc["location"] = {
                 "lat": doc_row["Latitude"],
-                "lon": doc_row["Longitude"]
+                "lon": doc_row["Longitude"],
             }
 
         for key in doc_row:
@@ -47,24 +48,23 @@ class CSVConnector:
             elif key == "Final Date":
                 es_doc[self._trim(key)] = self._convert_date(doc_row[key])
             elif key == "Initial Date":
-                es_doc[self._trim(key)] = self._convert_date(doc_row[key])   
+                es_doc[self._trim(key)] = self._convert_date(doc_row[key])
             else:
                 es_doc[self._trim(key)] = doc_row[key]
 
         self.elasticindexer.add_doc(es_doc)
 
-
     def _convert_date(self, date_string):
-        #2017-11-11 17:00:00
+        # 2017-11-11 17:00:00
         if not date_string:
             return None
 
-        timestamp = datetime.strptime(date_string, '%Y-%m-%d %H:%M:%S')
-        return timestamp.strftime("%Y-%m-%dT%H:%M:%S.00Z")       
+        timestamp = datetime.strptime(date_string, "%Y-%m-%d %H:%M:%S")
+        return timestamp.strftime("%Y-%m-%dT%H:%M:%S.00Z")
 
     def _trim(self, term):
-        #Returns a safe string for field names in elastic
-        return term.lower().replace(" ","_")
+        # Returns a safe string for field names in elastic
+        return term.lower().replace(" ", "_")
 
 
 class ElasticIndexer:
@@ -77,12 +77,13 @@ class ElasticIndexer:
 
     def __init__(self):
         self.es = Elasticsearch(
-            hosts=self.ELASTIC_URL, 
+            hosts=self.ELASTIC_URL,
             raise_on_error=False,
             http_auth=(self.USER, self.PASS),
             use_ssl=False,
-            verify_certs=True)
-        
+            verify_certs=True,
+        )
+
         self.actions = []
         self.log = logging.getLogger("elastic_indexer")
         self.total_documents = 0
@@ -91,29 +92,21 @@ class ElasticIndexer:
 
     def add_template(self):
         template = {
-                  "index_patterns": [
-                    f"{self.INDEX}*"
-                ],
-                "mappings": {
-                    "properties": {
-                        "location": {
-                            "type": "geo_point"
-                        }
-                    }
-                }
-            }
-        
+            "index_patterns": [f"{self.INDEX}*"],
+            "mappings": {"properties": {"location": {"type": "geo_point"}}},
+        }
+
         self.es.indices.put_template(name=self.INDEX, body=json.dumps(template))
 
     def add_doc(self, doc):
         action = {}
 
-        action['_op_type'] = 'index'
-        action['_index'] = self.INDEX
-        action['_type'] = self.TYPE
-        #action["_id"] = 
-        action['_source'] = doc
- 
+        action["_op_type"] = "index"
+        action["_index"] = self.INDEX
+        action["_type"] = self.TYPE
+        # action["_id"] =
+        action["_source"] = doc
+
         self.log.debug(f"Appending doc: {action}")
         self.actions.append(action)
 
@@ -128,19 +121,26 @@ class ElasticIndexer:
 
         if respons is 0:
             self.total_documents = self.total_documents + sucess
-            self.log.info(f"Indexed successful {sucess} documents - Total documents indexed {self.total_documents} - {sucess/(time.time()-self.start):.2f} docs/s")
+            self.log.info(
+                f"Indexed successful {sucess} documents - Total documents indexed {self.total_documents} - {sucess/(time.time()-self.start):.2f} docs/s"
+            )
         else:
-            self.log.error(f"Indexing error with {respons} documents, successful indexed {sucess} documents") 
+            self.log.error(
+                f"Indexing error with {respons} documents, successful indexed {sucess} documents"
+            )
 
         self.start = time.time()
         self.actions = []
 
 
-
 if __name__ == "__main__":
-    logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    logging.basicConfig(
+        stream=sys.stdout,
+        level=logging.INFO,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    )
 
     log = logging.getLogger("elastic_connector")
-    
+
     connector = CSVConnector()
     connector.run()
